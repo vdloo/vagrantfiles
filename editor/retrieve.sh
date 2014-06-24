8e0dfe814bc3e7baae3ea0fe71342b08f352fab1
Rick van de Loo <rickvandeloo@gmail.com>
2014-06-24 20:15:44 +0200

    small change

--- editor/retrieve.sh
+++ editor/retrieve.sh
@@ -1,13 +1,16 @@
 #!/bin/sh
 #example ./retrieve.sh -p 1234 -u vdloo -h example.com
 
-PORT=22
 unset USER
 unset REMOTEHOST
 while getopts ":p:u:h:" opt; do
 	case "$opt" in
 		p)
-			PORT="$OPTARG" ;;
+			PORT="$OPTARG" 
+			if [ $PORT == "-h" ] || [ $PORT == '-u' ]; then
+				PORT=22
+			fi
+			;;
 		u)
 			USER="$OPTARG" ;;
 		h)
@@ -17,8 +20,7 @@ done
 
 [ ! -t 0 ] && cp /vagrant/retrieve.sh /home/vagrant/
 
-#check if there are arguments
-if [ "$USER" ] && [ "$REMOTEHOST" ]; then
+if [ "$USER" != "-p" ] && [ "$USER" != '-u' ] && [ "$USER" != '-h' ] && [ "$REMOTEHOST" != "-p" ] && [ "$REMOTEHOST" != '-u' ] && [ "$REMOTEHOST" != '-h' ]; then
 	#make the ssh agent forward available for root during provisioning
 	SSH_FIX_FILE="/etc/sudoers.d/root_ssh_agent"
 	if [ ! -f  $SSH_FIX_FILE ]; then
@@ -26,13 +28,11 @@ if [ "$USER" ] && [ "$REMOTEHOST" ]; then
 		chmod 0440 $SSH_FIX_FILE
 	fi
 	su vagrant -c "ssh-keyscan -p $PORT $REMOTEHOST >> ~/.ssh/known_hosts"
-	if [ $(whoami) = "vagrant" ]; then
+	if [ "$(id -u)" != "0" ]; then
 		git clone ssh://$USER@$REMOTEHOST:$PORT/~/repo/dotfiles/dotfiles.git && (rm ~/.bashrc && find dotfiles/ -mindepth 1 -maxdepth 1 ! -name '.git' -exec ln -s {} ~ ';')
 	else
 		su vagrant -c "git clone ssh://$USER@$REMOTEHOST:$PORT/~/repo/dotfiles/dotfiles.git && (rm ~/.bashrc && find dotfiles/ -mindepth 1 -maxdepth 1 ! -name '.git' -exec ln -s {} ~ ';') || echo 'host system ssh agent not accepted, try manually running ./retrieve.sh'"
-
 	fi
 else 
 	[ ! -t 0 ] && echo 'no remote host specified, manually run the retrieve.sh script from within the vm'
 fi
-
