282948adc4456c3fe8c8f0440314b4b599525a9c
Rick van de Loo <rickvandeloo@gmail.com>
2014-07-03 10:11:15 +0000

    ssh-copy-id before provisioning from private repos

--- editor/retrieve.sh
+++ editor/retrieve.sh
@@ -19,10 +19,11 @@ fi
 [ ! -t 0 ] && cp /vagrant/retrieve.sh /home/vagrant/
 if [ "$USER" != "-p" ] && [ "$USER" != '-u' ] && [ "$USER" != '-h' ] && [ "$REMOTEHOST" != "-p" ] && [ "$REMOTEHOST" != '-u' ] && [ "$REMOTEHOST" != '-h' ]; then
 	if [ "$(id -u)" != "0" ]; then
+		ssh-keygen -b 4096 -f ~/.ssh/id_rsa -t rsa -N '' && ssh-copy-id "$USER@$REMOTEHOST -p $PORT"
 		git clone ssh://$USER@$REMOTEHOST:$PORT/~/repo/dotfiles.git && (
 			rm ~/.bashrc && find dotfiles/ -mindepth 1 -maxdepth 1 ! -name '.git' -exec ln -s {} ~ ';'
 			. ~/.repostrap.sh
-			source ~/.bashrc
+			. ~/.bashrc
 		)
 	else
 		#make the ssh agent forward available for root during provisioning
