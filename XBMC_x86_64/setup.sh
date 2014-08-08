9607c7b8c6cc175df0fc969eb20ddfd4cb96b131
Rick van de Loo <rickvandeloo@gmail.com>
2014-08-08 12:39:27 +0000

    deps

--- XBMC_x86_64/setup.sh
+++ XBMC_x86_64/setup.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
-apt-get -y install git
-apt-get -y build-dep xbmc
+sudo apt-get -y install git libxslt1-dev libssh-dev swig2.0 openjdk-7-jdk xinit x11-xserver-utils
+sudo apt-get -y build-dep xbmc
+git clone git://github.com/xbmc/xbmc.git
+make -C xbmc/lib/taglib
+sudo make -C xbmc/lib/taglib install
+./xbmc/bootstrap
+./xbmc/configure
+make xbmc -j 4
+sudo make xbmc install
