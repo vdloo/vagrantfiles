d68f190266bcdbbb1331cae3424b214bc962ae82
Rick van de Loo <rickvandeloo@gmail.com>
2014-08-11 17:34:45 +0200

    trusty-tahr box

--- XBMC_x86_64/setup.sh
+++ XBMC_x86_64/setup.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
-sudo apt-get -y install git libxslt1-dev libssh-dev swig2.0 openjdk-7-jdk xinit x11-xserver-utils
+sudo apt-get -y install git libxslt1-dev libssh-dev swig swig2.0 openjdk-7-jdk xinit x11-xserver-utils
 sudo apt-get -y build-dep xbmc
 git clone git://github.com/xbmc/xbmc.git
 make -C xbmc/lib/taglib
 sudo make -C xbmc/lib/taglib install
-(cd xbmc; ./bootstrap)
-(cd xbmc; ./configure)
-make xbmc -j 4
-sudo make xbmc install
+(cd xbmc; ./bootstrap; ./configure; make && make install)
