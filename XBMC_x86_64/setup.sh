a72a6a51699bf72d12882a2937f18946db7a21f9
Rick van de Loo <rickvandeloo@gmail.com>
2014-08-11 18:38:33 +0200

    gui = true

--- XBMC_x86_64/setup.sh
+++ XBMC_x86_64/setup.sh
@@ -5,3 +5,6 @@ git clone git://github.com/xbmc/xbmc.git
 make -C xbmc/lib/taglib
 sudo make -C xbmc/lib/taglib install
 (cd xbmc; ./bootstrap; ./configure; make && make install)
+
+#autologin as user vagrant in gui
+sed -i '$ d' /etc/init/tty1.conf; echo "exec /bin/login -f vagrant < /dev/tty1 > /dev/tty1 2>&1" >> /etc/init/tty1.conf
