965d0cac7458f01b4441285682a77955c6ae4fff
Rick van de Loo <rickvandeloo@gmail.com>
2014-08-21 13:34:24 +0000

    now use bootstrap.sh script from dotfiles repo
    to define what a base box is. also updated most boxes from precise64 to trusty64

---
+++ VirtualBox/build_essential_x86_64/setup.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+apt-get -y update && apt-get -y install vim-nox git build-essential autoconf libtool automake clang llvm manpages-dev gdb gdb-doc htop
---
+++ VirtualBox/latex_x86_64/setup.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+apt-get -y update && apt-get -y install vim-nox git texlive-full haskell-platform pandoc
