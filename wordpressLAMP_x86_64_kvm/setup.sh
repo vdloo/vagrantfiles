e207019c465003487aa3ff6a4a861c5c7b86eb5d
Rick van de Loo <rickvandeloo@gmail.com>
2014-09-17 05:08:47 -0400

    set up db during provision

--- wordpressLAMP_x86_64_kvm/setup.sh
+++ wordpressLAMP_x86_64_kvm/setup.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 type -p apt-get \
-	&& apt-get -y update && apt-get -y install vim-nox git wordpress mysql-server apache2
+	&& apt-get -y update && apt-get -y upgrade && apt-get -y install vim-nox git wordpress mysql-server apache2 php5 libapache2-mod-php5
 #type -p pacman \
 #	&& pacman --noconfirm -Syyu && pacman --noconfirm -S vim git
+ln -s /usr/share/wordpress /var/www/html/wordpress
+gzip -d /usr/share/doc/wordpress/examples/setup-mysql.gz
+bash /usr/share/doc/wordpress/examples/setup-mysql -n wordpress localhost
+chown -R www-data /usr/share/wordpress
