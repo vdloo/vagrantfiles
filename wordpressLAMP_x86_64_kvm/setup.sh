7e5787ad89ff9d9b35ba3957a43cd0ab1d7efc8f
Rick van de Loo <rickvandeloo@gmail.com>
2014-09-18 15:58:33 -0400

    scheme environment kvm box

--- wordpressLAMP_x86_64_kvm/setup.sh
+++ wordpressLAMP_x86_64_kvm/setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 type -p apt-get \
-	&& apt-get -y update && apt-get -y upgrade && apt-get -y install vim-nox git wordpress mysql-server apache2 php5 libapache2-mod-php5
+	&& apt-get -y update && apt-get -y upgrade && apt-get -y install vim-nox git wordpress mysql-server apache2 php5 libapache2-mod-php5 htop screen php5-cli
 #type -p pacman \
 #	&& pacman --noconfirm -Syyu && pacman --noconfirm -S vim git
 ln -s /usr/share/wordpress /var/www/html/wordpress
