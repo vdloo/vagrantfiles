56c53394505e0fa345df919bb2a7d78f133b0518
Rick van de Loo <rickvandeloo@gmail.com>
2014-09-24 07:43:07 +0000

    removed symlink from /vagrant dir

--- LAMP_CentOS_x86_64/development/setup.sh
+++ LAMP_CentOS_x86_64/development/setup.sh
@@ -1,1 +1,8 @@
-../setup.sh
+#!/bin/bash
+type -p yum \
+	&& (
+		yum -y install wget vim-enhanced git mysql-server mysql php php-mysql httpd;\
+		chkconfig httpd on ;\
+		chkconfig mysqld on ;\
+		apachectl start ;\
+	)
--- LAMP_CentOS_x86_64/setup.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-type -p yum \
-	&& (
-		yum -y install wget vim-enhanced git mysql-server mysql php php-mysql httpd;\
-		chkconfig httpd on ;\
-		chkconfig mysqld on ;\
-		apachectl start ;\
-	)
