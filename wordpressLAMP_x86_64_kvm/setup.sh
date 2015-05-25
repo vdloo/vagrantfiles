fc21bb667736f26a93f6de72c1f8dcea6728a1e0
Rick van de Loo <rickvandeloo@gmail.com>
2015-05-25 19:18:13 +0200

    Remove vagrant-kvm repos
    vagrant-kvm is dead: https://github.com/adrahon/vagrant-kvm/issues/276
    replace these boxes with vagrant-libvirt someday maybe.

--- wordpressLAMP_x86_64_kvm/setup.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-type -p apt-get \
-	&& apt-get -y update && apt-get -y upgrade && apt-get -y install vim-nox git wordpress mysql-server apache2 php5 libapache2-mod-php5 htop screen php5-cli
-#type -p pacman \
-#	&& pacman --noconfirm -Syyu && pacman --noconfirm -S vim git
-ln -s /usr/share/wordpress /var/www/html/wordpress
-gzip -d /usr/share/doc/wordpress/examples/setup-mysql.gz
-bash /usr/share/doc/wordpress/examples/setup-mysql -n wordpress localhost
-chown -R www-data /usr/share/wordpress
