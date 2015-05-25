b24cd245ac5a26be3cb18e754aeb49b6a5d86602
Rick van de Loo <rickvandeloo@gmail.com>
2015-05-25 19:17:33 +0200

    fail when git repo exists

--- devenv/provision.sh
+++ devenv/provision.sh
@@ -4,8 +4,8 @@ puppet module install puppetlabs-vcsrepo
 puppet module install maestrodev-wget
 
 mkdir -p code/configs -m 755
-git clone git@github.com:vdloo/puppetfiles.git code/configs/puppetfiles 2> /dev/null
-(cd code/configs/puppetfiles; bash manifests/headless.pp --verbose)
+git clone https://github.com/vdloo/puppetfiles code/configs/puppetfiles
+(cd code/configs/puppetfiles; bash papply.sh manifests/headless.pp --verbose)
 
 # This needs to happen outside of puppet because Vundle requires a tty to
 # install :( see: https://github.com/gmarik/Vundle.vim/issues/511
