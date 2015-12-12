19a9dc536387b5b4be7782ceda7c8cc4bf6c8fcd
Rick van de Loo <rickvandeloo@gmail.com>
2015-12-12 19:49:46 +0100

    remove image maintainer's user

--- workstation/provision.sh
+++ workstation/provision.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env sh
+userdel terry	# remove image maintainer's user
 pacman -Syyu --noconfirm && pacman -S git puppet acl --noconfirm
 (cd /usr/etc/puppetfiles; bash papply.sh manifests/workstation.pp --verbose)
