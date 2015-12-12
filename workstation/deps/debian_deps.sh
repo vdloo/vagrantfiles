38d600bdd555d7c2afc495b84bdfb44b805c3ca3
Rick van de Loo <rickvandeloo@gmail.com>
2015-12-12 17:43:26 +0100

    vagrantfiles only provide archlinux boxes
    no more switching with VAGRANT_DISTRO as env var, removing some
    complexity because I rarely use my puppetfiles on debian/ubuntu anyway

--- workstation/deps/debian_deps.sh
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/env sh
-apt-get -y install virtualbox-guest-x11
--- workstation/provision.sh
+++ workstation/provision.sh
@@ -1,2 +1,3 @@
 #!/usr/bin/env sh
+pacman -Syyu --noconfirm && pacman -S git puppet acl --noconfirm
 (cd /usr/etc/puppetfiles; bash papply.sh manifests/workstation.pp --verbose)
