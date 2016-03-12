8d5dfe9ecf2baac4d94a5145512cfae9b9a4bff8
Rick van de Loo <rickvandeloo@gmail.com>
2016-03-12 11:08:00 +0100

    mount synced dir over /home
    so there is enough space to build things

--- headless/provision.sh
+++ headless/provision.sh
@@ -1,4 +1,11 @@
 #!/usr/bin/env sh
+
+# mount shared shared dir over /home so we don't run out of disk space
+NONROOT_USER=$(id -u vdloo > /dev/null 2>&1 && echo 'vdloo' || echo 'nonroot')
+mkdir -p "/vagrant/home/$NONROOT_USER"
+mkdir -p "/home/$NONROOT_USER"
+mount --bind "/vagrant/home/$NONROOT_USER" "/home/$NONROOT_USER"
+
 puppet module install puppetlabs-vcsrepo
 puppet module install maestrodev-wget
 puppet module install saz-sudo
@@ -11,8 +18,3 @@ git clone https://github.com/vdloo/puppetfiles /usr/etc/puppetfiles
 # This needs to happen outside of puppet because Vundle requires a tty to
 # install :( see: https://github.com/gmarik/Vundle.vim/issues/511
 #su vdloo -c vim +PluginInstall +qall; printf "\033c"
-
-NONROOT_USER=$(id -u vdloo 2>&1> /dev/null && echo 'vdloo' || echo 'nonroot')
-echo "setfacl -m $NONROOT_USER:x \$(dirname \"\$SSH_AUTH_SOCK\")" >> /home/vagrant/.bashrc
-echo "setfacl -m $NONROOT_USER:rwx \"\$SSH_AUTH_SOCK\"" >> /home/vagrant/.bashrc
-echo "sudo su $NONROOT_USER -l -c \"export SSH_AUTH_SOCK=\$SSH_AUTH_SOCK; bash\"" >> /home/vagrant/.bashrc
