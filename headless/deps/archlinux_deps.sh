81a9b0a3121ad12c5e8ccc6ec54d5be0bc0baeec
Rick van de Loo <rickvandeloo@gmail.com>
2016-05-01 13:41:57 +0200

    install latest ruby before running puppet
    fixes:
    ```
    ==> default: Cloning into '/usr/etc/puppetfiles'...
    ==> default: cannot load such file -- puppet/util/command_line
    ==> default: cannot load such file -- puppet/util/command_line
    ```

--- headless/deps/archlinux_deps.sh
+++ headless/deps/archlinux_deps.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env sh
 pacman -Syyu --noconfirm
-pacman -S git puppet --noconfirm
+pacman -S ruby git puppet --noconfirm
 pacman -S acl --noconfirm
--- htpc/provision.sh
+++ htpc/provision.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env sh
 userdel terry	# remove image maintainer's user
-pacman -Syyu --noconfirm && pacman -S git puppet xf86-video-vesa --noconfirm
+pacman -Syyu --noconfirm && pacman -S ruby git puppet xf86-video-vesa --noconfirm
 
 # this must be the last statement because we need to reboot after initial installation
 # so we autologin into the desktop environment
