024b60f48c7bec92a28f81ed293128934ab7d5b4
Rick van de Loo <rickvandeloo@gmail.com>
2016-07-23 23:30:01 +0200

    remove conflicting dkms

--- gui/provision.sh
+++ gui/provision.sh
@@ -1,2 +1,4 @@
 #!/usr/bin/env sh
+# remove conflicting guest-dkms
+pacman -Rsc virtualbox-guest-dkms --noconfirm
 pacman -Syyu --noconfirm && pacman -S ruby git puppet acl xf86-video-vesa --noconfirm
