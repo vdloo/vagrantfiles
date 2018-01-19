e7439a35a5561626e58964584c4570c7189841f4
Rick van de Loo <rickvandeloo@gmail.com>
2018-01-19 07:55:56 +0100

    use --needed in pacman package installs

--- gui/provision.sh
+++ gui/provision.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env sh
 # remove conflicting guest-dkms
-pacman -Rsc virtualbox-guest-dkms --noconfirm
-pacman -Syyu --noconfirm && pacman -S ruby git puppet acl xf86-video-vesa --noconfirm
+pacman -Rsc virtualbox-guest-dkms --noconfirm --needed
+pacman -Syyu --noconfirm && pacman -S ruby git puppet acl xf86-video-vesa --noconfirm --needed
+
--- headless/deps/archlinux_deps.sh
+++ headless/deps/archlinux_deps.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env sh
 pacman -Sy --noconfirm
-pacman -S ruby git puppet --noconfirm
-pacman -S acl --noconfirm
+pacman -S ruby git puppet acl --noconfirm --needed
 
 # Make sure the kernel is not upgraded on 'pacman -Syu' because otherwise we'd need to reboot
 # yet again before docker will work inside the virtualized guest due to the right veth kernel module 
@@ -9,3 +8,4 @@ pacman -S acl --noconfirm
 # docker: Error response from daemon: failed to create endpoint ... on network bridge: 
 # failed to add the host (...) <=> sandbox (...) pair interfaces: operation not supported.
 grep -q "IgnorePkg = linux" "/etc/pacman.conf" || sed -i '/\[options\]/a IgnorePkg = linux' /etc/pacman.conf 
+
